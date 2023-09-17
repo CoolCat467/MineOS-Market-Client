f5cae603c4c01bb3947271a82daa641ff42f0eed
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2023-09-17 00:50:07 -0500

    Fix table fields and implement function call parsing

--- src/lua_parser.py
+++ src/lua_parser.py
@@ -10,6 +10,7 @@ __version__ = "0.0.0"
 
 
 import re
+from collections import deque
 from collections.abc import Collection
 from typing import Any, Generic, NamedTuple, NoReturn, TypeVar, cast
 
@@ -33,6 +34,16 @@ class Token(NamedTuple):
     text: str
     line: int
     column: int
+
+    @property
+    def endcolumn(self) -> int:
+        """End column number."""
+        return self.column + len(self.text)
+
+    @property
+    def endline(self) -> int:
+        """End line number."""
+        return self.line + self.text.count("\n")
 
 
 class Identifier(Token):
@@ -127,7 +138,7 @@ HEXADECIMAL = re.compile(
 def tokenize(text: str) -> list[Token]:
     """Tokenize lua code."""
     line = 1
-    column = 0
+    column = -1
 
     tokens: list[Token] = []
     while True:
@@ -142,7 +153,7 @@ def tokenize(text: str) -> list[Token]:
         elif text[0] == "\n":
             token = None
             line += 1
-        elif text[0] in "()[]{},":
+        elif text[0] in "()[]{},;":
             token = Separator
         elif text.startswith("--"):
             match_ = COMMENT.match(text)
@@ -166,15 +177,7 @@ def tokenize(text: str) -> list[Token]:
             token = StrLit
         else:
             if match_ := (HEXADECIMAL.match(text) or NUMERIC.match(text)):
-                tokens.append(
-                    Numeric(
-                        "$".join(x or "" for x in match_.groups()),
-                        line,
-                        column,
-                    ),
-                )
-
-                token = None
+                token = Numeric
                 read = match_.end()
             elif match_ := IDENTIFIER.match(text):
                 token = Identifier
@@ -184,7 +187,7 @@ def tokenize(text: str) -> list[Token]:
                 raise ParseError(f"Could not parse {text!r}")
 
         if last_line != line:
-            column = 0
+            column = -1
         else:
             column += read
 
@@ -222,11 +225,14 @@ class Value(Generic[T]):
 
     def __str__(self) -> str:
         """Return type value representation of self."""
-        args = tuple(map(str, self.args))
-        if not args:
-            if self.name:
-                return self.name
-            return "[]"
+        if not self.args:
+            return self.name or "[]"
+        args = []
+        for arg in self.args:
+            if isinstance(arg, Value):
+                args.append(str(arg))
+            else:
+                args.append(repr(arg))
         values = ", ".join(args)
         return f"{self.name}[{values}]"
 
@@ -273,12 +279,13 @@ def list_or(values: Collection[str]) -> str:
 class Parser:
     """Implementation of the type comment parser."""
 
-    __slots__ = ("tokens", "i")
+    __slots__ = ("tokens", "i", "next_indexed_field")
 
     def __init__(self, tokens: list[Token]) -> None:
         """Initialize with tokens list."""
         self.tokens = tokens
         self.i = 0
+        self.next_indexed_field: deque[int] = deque()
 
     def fail(self, error: str | None) -> NoReturn:
         """Raise parse error."""
@@ -319,6 +326,15 @@ class Parser:
         got = self.next().text
         if got != text:
             self.fail(f"Expected {text!r}, got {got!r}")
+
+    def expect_or(self, options: Collection[str]) -> Token:
+        """Expect next token text to be text. Return the token we got."""
+        token = self.next()
+        if token.text not in options:
+            self.fail(
+                f"Expected {list_or([repr(x) for x in options])}, got {token.text!r}",
+            )
+        return token
 
     def expect_type(
         self,
@@ -364,14 +380,15 @@ class Parser:
                 },
             )
             skip = 1
-        return Value("String", value)
+        return Value("String", value[1:-1])
 
     def parse_numeric_literal(self) -> Value[int | float]:
         """Parse a numeric literal."""
         token = self.expect_type(Numeric)
 
-        decimal, fractional, exponent = token.text.split("$")
-        text = "".join((decimal, fractional, exponent))
+        text = token.text
+        match_ = HEXADECIMAL.match(text) or NUMERIC.match(text)
+        decimal, fractional, exponent = match_.groups()
 
         is_float = fractional or exponent
 
@@ -392,22 +409,36 @@ class Parser:
             return Value("Field", value, self.parse_value())
         if isinstance(self.peek(), Identifier):
             return self.parse_identifier()
-        return Value("Indexed", self.parse_value())
+        index = self.next_indexed_field.pop()
+        self.next_indexed_field.append(index + 1)
+        # return Value("Indexed", self.parse_value())
+        return Value("Field", Value("Integer", index), self.parse_value())
 
     def parse_table(self) -> Value[object]:
         """Parse table."""
         self.expect("{")
+        self.next_indexed_field.append(1)
         fields = []
         while self.lookup() != "}":
             fields.append(self.parse_field())
 
-            while string := self.lookup():
-                if string in {",", ";"}:
-                    self.expect(string)
-                else:
-                    break
+            if self.expect_or({",", ";", "}"}).text == "}":
+                self.back()
+        self.next_indexed_field.pop()
         self.expect("}")
         return Value("Table", *fields)
+
+    def parse_function_arguments(self) -> list[Value[object]]:
+        """Parse function call arguments."""
+        self.expect("(")
+        arguments = []
+        while self.lookup() != ")":
+            arguments.append(self.parse_value())
+
+            if self.expect_or({",", ")"}).text == ")":
+                self.back()
+        self.expect(")")
+        return arguments
 
     def parse_identifier(self) -> Value[object]:
         """Parse identifier."""
@@ -417,12 +448,36 @@ class Parser:
             return Value("Boolean", text == "true")
         if text in KEYWORDS:
             return Value("Keyword", text)
-        self.expect_type(Assignment)
-        return Value(
-            "Assignment",
-            Value("Identifier", text),
-            self.parse_value(),
-        )
+        # Function calls are strange.
+        if self.lookup() == "(":  # Regular function call
+            return Value(
+                "FunctionCall",
+                Value("Identifier", text),
+                Value("Arguments", *self.parse_function_arguments()),
+            )
+        if self.lookup() == "{":  # syntactic sugar call from table constructor
+            return Value(
+                "FunctionCall",
+                Value("Identifier", text),
+                Value("Arguments", *self.parse_table()),
+            )
+        if isinstance(
+            self.peek(),
+            StrLit,
+        ):  # syntactic sugar call from string literal
+            return Value(
+                "FunctionCall",
+                Value("Identifier", text),
+                Value("Arguments", *self.parse_string_literal()),
+            )
+        if isinstance(self.peek(), Assignment):
+            self.expect_type(Assignment)
+            return Value(
+                "Assignment",
+                Value("Identifier", text),
+                self.parse_value(),
+            )
+        return Value("Identifier", text)
 
     def parse_value(self) -> Value[Any]:
         """Parse value."""
@@ -437,7 +492,7 @@ class Parser:
         raise NotImplementedError(self.peek())
 
 
-def parse_lua_table(text: str) -> object:
+def parse_lua_table(text: str, convert_lists: bool = True) -> object:
     """Parse lua table from lua source."""
     tokens = tokenize(text)
     # print(f'{tokens = }')
@@ -473,16 +528,16 @@ def parse_lua_table(text: str) -> object:
     def read_field(
         value: Value[object],
         table: dict[str | int, object],
-    ) -> tuple[tuple[str | int, object], bool]:
+    ) -> tuple[str | int, object]:
         """Read a table field value."""
-        assert value.name in {"Field", "Assignment", "Indexed"}
+        assert value.name in {"Field", "Assignment"}
         if value.name == "Assignment":
-            return read_assignment(value), False
-        if value.name == "Indexed":
-            return (len(table), read_value(value.args[0])), True
+            return read_assignment(value)
+        # if value.name == "Indexed":
+        #    return (str(len(table)), read_value(value.args[0]))
         if value.name == "Field":
             field, field_value = value.args
-            return (read_value(field), read_value(field_value)), False
+            return (read_value(field), read_value(field_value))
         raise NotImplementedError(value.name)
 
     def read_table(
@@ -491,16 +546,23 @@ def parse_lua_table(text: str) -> object:
         """Read a table and all of it's fields."""
         assert value.name == "Table"
         table: dict[str | int, object] = {}
-        all_indexed = True
+
+        last_int_key = 0
+        convert_list = convert_lists
+
         for field in value.args:
-            pair, indexed = read_field(field, table)
-            if not indexed:
-                all_indexed = False
-            key, store_value = pair
+            key, store_value = read_field(field, table)
+            if convert_list:
+                if not isinstance(key, int):
+                    convert_list = False
+                elif key == (last_int_key + 1):
+                    last_int_key = key
+                else:
+                    convert_list = False
             table[key] = store_value
-        if all_indexed:
-            return [table[i] for i in range(len(table))]
-        return table
+        if not convert_list:
+            return table
+        return [table[i + 1] for i in range(len(table))]
 
     return read_value(value)
     # return value.unpack_join()
@@ -514,60 +576,64 @@ def run() -> None:
  a = '\\97lo\\10\\04923"'
  c = 0X1.921FB54442D18P+1
  j = 314.16e-2
- e = 0xBEBADA""",
+ e = 0xBEBADA
+ a = { [f(1)] = g; "x", "y"; x = 1, f(x), [30] = 23; 45 }""",
     )
-    ##print(f'{tokens = }')
+    # print(f'{tokens = }')
+    ##for token in tokens:
+    ##    print(token)
+    global parser
     parser = Parser(tokens)
-    for _ in range(6):
-        print(repr(parser.parse_identifier()))
-
-    print(
-        parse_lua_table(
-            """{
-    help = "Help",
-    helpInfo = {
-        "• Every model consists of shapes. Their count depends of server properties, but by default it's 24 per one model",
-        "• Model also can have 2 states: passive and active. If you place printed model in minecraft world and perform a click on it, model will change it's state to the opposite. Shape count is limited to both states",
-        "• Hold left mouse button to place first point of shape by X and Y. Drag mouse to resize shape. Use mouse wheel to change Z coordinate. Release button to finish shape editing",
-        "• Press right mouse button to select shape under cursor",
-        "• Scroll toolbar to show other options",
-    },
-    file = "File",
-    new = "New",
-    open = "Open",
-    save = "Save",
-    saveAs = "Save as",
-    disabled = "Disabled",
-    enabled = "Enabled",
-    add = "Add",
-    remove = "Remove",
-    rotate = "Rotate",
-    flip = "Flip",
-    color = "C",
-    offset = "Offset",
-    scale = "Scale",
-    projectorEnabled = "Enabled",
-    xAxis = "X-axis",
-    yAxis = "Y-axis",
-    zAxis = "Z-axis",
-    lightLevel = "Light level",
-    emitRedstone = "Emit redstone",
-    collidable = "Collision",
-    buttonMode = "Button mode",
-    label = "Label",
-    tooltip = "Tooltip",
-    tintEnabled = "Use tint",
-    tintColor = "Tint color",
-    texture = "Texture",
-    print = "Print",
-    blockSettings = "Model properties",
-    projectorSettings = "Projector properties",
-    elementSettings = "Shape properties",
-    failedToPrint = "Failed to print",
-}""",
-        ),
-    )
-
+    for _ in range(7):
+        print(str(parser.parse_identifier()))
+    ##
+    ##    print(
+    ##        parse_lua_table(
+    ##            """{
+    ##    help = "Help",
+    ##    helpInfo = {
+    ##        "• Every model consists of shapes. Their count depends of server properties, but by default it's 24 per one model",
+    ##        "• Model also can have 2 states: passive and active. If you place printed model in minecraft world and perform a click on it, model will change it's state to the opposite. Shape count is limited to both states",
+    ##        "• Hold left mouse button to place first point of shape by X and Y. Drag mouse to resize shape. Use mouse wheel to change Z coordinate. Release button to finish shape editing",
+    ##        "• Press right mouse button to select shape under cursor",
+    ##        "• Scroll toolbar to show other options",
+    ##    },
+    ##    file = "File",
+    ##    new = "New",
+    ##    open = "Open",
+    ##    save = "Save",
+    ##    saveAs = "Save as",
+    ##    disabled = "Disabled",
+    ##    enabled = "Enabled",
+    ##    add = "Add",
+    ##    remove = "Remove",
+    ##    rotate = "Rotate",
+    ##    flip = "Flip",
+    ##    color = "C",
+    ##    offset = "Offset",
+    ##    scale = "Scale",
+    ##    projectorEnabled = "Enabled",
+    ##    xAxis = "X-axis",
+    ##    yAxis = "Y-axis",
+    ##    zAxis = "Z-axis",
+    ##    lightLevel = "Light level",
+    ##    emitRedstone = "Emit redstone",
+    ##    collidable = "Collision",
+    ##    buttonMode = "Button mode",
+    ##    label = "Label",
+    ##    tooltip = "Tooltip",
+    ##    tintEnabled = "Use tint",
+    ##    tintColor = "Tint color",
+    ##    texture = "Texture",
+    ##    print = "Print",
+    ##    blockSettings = "Model properties",
+    ##    projectorSettings = "Projector properties",
+    ##    elementSettings = "Shape properties",
+    ##    failedToPrint = "Failed to print",
+    ##}""",
+    ##        ),
+    ##    )
+    ##
     print(
         parse_lua_table(
             """{
@@ -587,6 +653,192 @@ def run() -> None:
     population = "Population: ",
     city = "Type city name here",
     cityError = "Wrong result. Check city name and try again."
+}""",
+        ),
+    )
+
+    print(
+        parse_lua_table(
+            """{
+    leftBarOffset = 5,
+
+    settingsStyle = "Color scheme",
+    settingsAdditional = "Additional properties",
+
+    invalidPassword = "Invalid login or password",
+    username = "E-mail or phone number",
+    password = "Password",
+    login = "Enter",
+    twoFactorEnabled = "Use 2FA",
+    twoFactor = "2FA code",
+    saveLogin = "Save login information:",
+
+    loadCountConversations = "The number of loaded dialogues",
+    loadCountMessages = "Number of messages to download",
+    loadCountNews = "Number of downloaded news",
+    scrollSpeed = "The scroll speed",
+    loadCountWall = "Number of posts to download",
+    loadCountFriends = "Number of friends to download",
+    loadCountDocs = "Number of documents to download",
+
+    profile = "Profile",
+    message = "Message",
+    sendMessage = "Send message",
+    addToFriends = "Add as friend",
+    friendRequestSent = "Request has been sent",
+    userSubscribedToYou = "Subscribed to you",
+    userIsYourFriend = "You have friends",
+
+    profileCounters = {
+        { field = "followers", description = " followers" },
+        { field = "friends", description = " friends" },
+        { field = "photos", description = " photos" },
+        { field = "videos", description = " videos" },
+        { field = "audios", description = " audio" },
+    },
+    profileShowAdditional = "Show details",
+    profileHideAdditional = "Hide detailed information",
+    profileTitleMainInformation = "Basic information",
+    profileTitlePersonal = "Life position",
+    profileTitleAdditions = "Personal information",
+    profileTitleContacts = "Contacts",
+    profileKeys = {
+        education = "Education",
+        inspiredBy = "Inspire",
+        relation = "Relations",
+        birthday = "Birthday",
+        city = "City",
+        homeCity = "Hometown",
+        languages = "Languages",
+        occupation = "Place of work",
+        mobilePhone = "Mobile",
+        homePhone = "Alt. phone",
+        site = "Website",
+        activities = "Activity",
+        interests = "Interests",
+        music = "Favorite music",
+        movies = "Favorite movie",
+        books = "Favorite book",
+        tv = "Favorite show",
+        games = "Favorite game",
+        quotes = "Favorite quote",
+        religion = "Outlook",
+        political = "Polit. conviction",
+        peopleMain = "People most important",
+        lifeMain = "Life most important",
+        alcohol = "OTN. for alcohol",
+        smoking = "OTN. towards smoking",
+    },
+    months = {
+        "january",
+        "february",
+        "march",
+        "april",
+        "may",
+        "june",
+        "july",
+        "august",
+        "september",
+        "october",
+        "november",
+        "december",
+    },
+    relationStatuses = {
+        {
+            "not married",
+            "have a girlfriend",
+            "engaged",
+            "married",
+            "it's complicated",
+            "in active search",
+            "lover",
+            "in civil marriage",
+            [0] = "unspecified",
+        },
+        {
+            "not married",
+            "have a friend",
+            "engaged",
+            "married",
+            "it's complicated",
+            "in active search",
+            "lover",
+            "in civil marriage",
+            [0] = "unspecified",
+        }
+    },
+    personalPoliticalTypes = {
+        "Communist",
+        "Socialist",
+        "Moderate",
+        "Liberal",
+        "Conservative",
+        "Monarchical",
+        "Ultraconservative",
+        "Indifferent",
+        "Libertarian",
+    },
+    personalPeopleMainTypes = {
+        "Intelligence and creativity",
+        "Kindness and honesty",
+        "Beauty and health",
+        "Power and wealth",
+        "Courage and perseverance",
+        "Humor and love of life",
+    },
+    personalLifeMainTypes = {
+        "Family and children",
+        "Career and money",
+        "Entertainment and recreation",
+        "Science and research",
+        "Improving the world",
+        "Self-development",
+        "Beauty and art",
+        "Fame and influence",
+    },
+    personalBlyadTypes = {
+        "Sharply negative",
+        "Negative",
+        "Compromise",
+        "Neutral",
+        "Positive",
+    },
+
+    friends = "Friends",
+    friendsOnline = "Online friends",
+    friendsMutual = "Mutual friends",
+
+    news = "Newsfeed",
+
+    send = "Send",
+    conversations = "Conversations",
+    you = "You: ",
+    typeMessageHere = "Write a message...",
+    fwdMessages = "Information: ",
+    attachments = "Investments: ",
+    attachmentsTypes = {
+        photo = "photo",
+        video = "video",
+        audio = "audio",
+        doc = "document",
+        link = "link",
+        market = "goods",
+        market_album = "product catalog",
+        wall = "record",
+        wall_reply = "repost",
+        sticker = "sticker",
+        gift = "gift",
+        audio_message = "audio",
+    },
+    sentFromMineOS = " (Sent via MineOS VK Client)",
+
+    documents = "Documents",
+    documentsCount = "Total documents",
+    documentsAdd = "Upload document",
+
+    settings = "Settings",
+
+    exit = "Logout",
 }""",
         ),
     )
